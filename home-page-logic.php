3e0a9b6c642ff117869d44a134fcb32288cddc03
Marcin <marcinbureta@gmail.com>
2025-02-04 21:35:51 +0100

    fixed typo

--- home-page-logic.php
+++ home-page-logic.php
@@ -5,11 +5,11 @@ session_start();
 // account button either points to about user page or login page
 // depending if user logged in or not
 document.addEventListener("DOMContentLoaded", function(){
-    const isLOggedIn = "<?php isset($_SESSION['userID']) ? 'true' : 'false';?>";
+    const isLoggedIn = "<?php echo isset($_SESSION['userID']) ? 'true' : 'false';?>";
     const account_btn = document.querySelector(".login-btn");
     if(account_btn){
         account_btn.addEventListener("click", function(){
-            window.location.href = isLOggedIn ? "about_account.php" : "login.php";
+            window.location.href = isLoggedIn ? "about_account.php" : "login.php";
         })
     }
 })
