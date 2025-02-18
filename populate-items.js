ed91a8745f2bd779124a10cdf653c6b909c93d2f
Marcin <marcinbureta@gmail.com>
2025-02-18 23:26:59 +0100

    added database and js script to populate items

--- populate-items.js
+++ populate-items.js
@@ -1,6 +1,20 @@
 // creates items and populates home page
 const container = document.querySelector("#home-page-products");
 
-function createElement(image, description, price){
-    let itemCard = document.createElement("div");
+function createElement(itemID){
+    let itemContainer = document.createElement("div");
+    itemContainer.classList.add("home-item-container");
+    let imageContainer = document.createElement("div");
+    imageContainer.classList.add("home-image-container");
+    let image = document.createElement("img");
+    image.classList.add("home-image");
+
+    let itemDescription = document.createElement("div");
+    itemDescription.classList.add("home-item-description");
+    let priceTag = document.createElement("div");
+    priceTag.classList.add("home-price-tag");
+    let addToCartBtn = document.createElement("button");
+    addToCartBtn.classList.add("home-add-to-cart-btn");
 }
+
+// 
