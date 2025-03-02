37040fa6727341e8be1ff60481c053e5c8fa5702
Marcin <marcinbureta@gmail.com>
2025-03-02 16:58:10 +0100

    added animation to sorting items function so it wont stutter now

--- populate_items_category.js
+++ populate_items_category.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function(){
     document.querySelectorAll(".category-card-btn").forEach(category=>{
         category.addEventListener("click", function(){
             let category_name = this.dataset.category;
-
             fetch(`get_category_items.php?item_type=${encodeURIComponent(category_name)}`)
             .then(response => response.json())
             .then(items => {
@@ -13,16 +12,18 @@ document.addEventListener("DOMContentLoaded", function(){
                 items.forEach(item => {
                     container.appendChild(createElement(item.image_path,
                         item.description, item.price, item.itemID));
-                })
-                //calculates new content height and then sets it into container as value in px
-                container.style.height = "auto";
-                let newHeight = container.scrollHeight + "px";
-                container.style.height = current_height;
-                setTimeout(() => {
-                    container.style.height = newHeight;
-                }, 100);
+                });
+                setTimeout(() =>{
+                    // auto height sets height so elements would fit
+                    container.style.height = "auto";
+                    let newHeight = container.scrollHeight + "px";
+                    container.style.height = current_height;
+                    setTimeout(() =>{
+                        container.style.height = newHeight;
+                    },100);
+                },100);
             }).catch(error => {
-                console.error("Error fetching shop items:", error) 
+                console.error("Error fetching shop items:", error); 
             })
         })
     })
