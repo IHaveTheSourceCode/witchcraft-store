e0d996c778f3b54c223086a501b4d79e58885b41
Marcin <marcinbureta@gmail.com>
2025-03-25 01:54:28 +0100

    removed database connection as function parameter and added it as global param

--- add_to_cart_logic.php
+++ add_to_cart_logic.php
@@ -1,31 +1,41 @@
 <?php
-// include "dbconnect.php";
+include "dbconnect.php";
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
     session_regenerate_id(true);
 };
 
-function createCart($user_id, $conn) {
+function createCart($user_id) {
+    global $conn;
     $stmt = $conn->prepare("INSERT INTO carts (user_id) VALUES (?)");
     $stmt->bind_param("i", $user_id);
     $stmt->execute();
 }
 
-function addItemToCart($cart_id, $product_id, $quantity, $conn) {
+// checks if user has any cart present already
+function checkCartPresence($user_id){
+    global $conn;
+    $stmt = $conn->prepare
+}
+
+function addItemToCart($cart_id, $product_id, $quantity){
+    global $conn;
     $stmt = $conn->prepare("INSERT INTO cart_items (cart_id, product_id, quantity) 
     VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE quantity = quantity + ?");
     $stmt->bind_param("iiii", $cart_id, $product_id, $quantity, $quantity);
     $stmt->execute();
 }
 
-function deleteItemFromCart($cart_id, $product_id, $conn){
+function deleteItemFromCart($cart_id, $product_id){
+    global $conn;
     $stmt = $conn->prepare("DELETE FROM cart_items WHERE cart_id = ? AND product_id = ?");
     $stmt->bind_param("ii", $cart_id, $product_id);
     $stmt->execute();
 }
 
-function getCartItems($cart_id, $conn) {
+function getCartItems($cart_id){
+    global $conn;
     //joins shop_items and cart_items tables together by same productID where 
     //cart_itemsID coresponds to selected user
     $stmt = $conn->prepare("SELECT items.name, items.price, c.quantity FROM cart_items c 
@@ -35,7 +45,8 @@ function getCartItems($cart_id, $conn) {
     return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 }
 
-function getCartID($user_id, $conn){
+function getCartID($user_id){
+    global $conn;
     $stmt = $conn->prepare("SELECT id from carts WHERE user_id = ?");
     $stmt->bind_param("i", $user_id);
     $stmt->execute();
