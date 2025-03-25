cd6311c1a78e1f1d9a3fd92ae2cef6140727a375
Marcin <marcinbureta@gmail.com>
2025-03-25 01:28:02 +0100

    added cart interactions

--- home-page-logic.php
+++ home-page-logic.php
@@ -5,15 +5,24 @@ if (session_status() === PHP_SESSION_NONE){
 }
 ?>
 <script>
-// account button either points to about user page or login page
-// depending if user logged in or not
 document.addEventListener("DOMContentLoaded", function(){
     const isLoggedIn = "<?php echo isset($_SESSION['userID']) ? 'true' : 'false';?>" === "true";
+    // account button either points to about user page or login page
+    // depending if user logged in or not
     const account_btn = document.querySelector(".login-btn");
     if(account_btn){
         account_btn.addEventListener("click", function(){
             window.location.href = isLoggedIn ? "about_account.php" : "login.php";
         })
     }
+    // cart button shows alert window that user is not logged in if he's not
+    const cart_btn = document.querySelector(".cart-btn");
+    cart_btn.addEventListener("click", function(){
+        if(isLoggedIn){}
+        else{
+            alert("Please log in first to access cart.");
+        }
+    })
 })
+
 </script>
